9d172083a8da09500dd08dd1c11976c0e8b338fc
Farhan Noor Abir <fnoor.abir@gmail.com>
2026-06-12 15:02:29 +0100

    Update the transaction pages in mobile

--- apps/mobile/src/app/project/[id].tsx
+++ apps/mobile/src/app/project/[id].tsx
@@ -1,21 +1,31 @@
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Pressable, TouchableOpacity } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
-import { getDatabaseReference, getTotalValue, updateBalance } from '@repo/app';
+import {
+  formatCurrency,
+  getDatabaseReference,
+  getTotalValue,
+  updateBalance,
+} from '@repo/app';
 import { useList, useObject } from 'react-firebase-hooks/database';
 import {
   EmptyUI,
   ErrorUI,
+  ProjectTransactionDialog,
   ProjectTransactionRow,
+  SelectOption,
   toast,
   TotalBalanceRow,
 } from '@repo/ui';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { HeaderBar } from '@/src/components/HeaderBar';
 import { Loading } from '@/src/components/Loading';
+import { ThemedIcon } from '@/src/components/ThemedIcon';
 
 export default function ProjectDetailScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
+
+  const [openDialog, setOpenDialog] = useState(false);
 
   const [data, transactionLoading, transactionError] = useList(
     getDatabaseReference(`transaction/project/${id}`),
@@ -38,10 +48,29 @@ export default function ProjectDetailScreen() {
     getDatabaseReference(`balance/project/${id}`),
   );
 
+  const billData = useMemo(
+    () => uniqueData.filter((item) => item.val().amount >= 0),
+    [uniqueData],
+  );
+  const paymentData = useMemo(
+    () => uniqueData.filter((item) => item.val().amount < 0),
+    [uniqueData],
+  );
+
+  const totalBill = useMemo(
+    () => getTotalValue(billData, 'amount'),
+    [billData],
+  );
+  const totalPayment = useMemo(
+    () => getTotalValue(paymentData, 'amount'),
+    [paymentData],
+  );
+
   const balanceVal = balance?.val();
-  const total = useMemo(() => {
-    return data ? getTotalValue(data, 'amount') : 0;
-  }, [data]);
+  const total = useMemo(
+    () => totalBill + totalPayment,
+    [totalBill, totalPayment],
+  );
   const totalValue = balanceVal?.value ?? 0;
 
   const loading = transactionLoading || balanceLoading;
@@ -67,46 +96,92 @@ export default function ProjectDetailScreen() {
     syncBalance();
   }, [total, totalValue, loading, error, id]);
 
+  const paidDataOptions: SelectOption[] | undefined = useMemo(
+    () =>
+      uniqueData
+        ?.filter((t) => t.val().amount < 0)
+        .sort((a, b) => b.key!.localeCompare(a.key!))
+        .map((item) => ({
+          value: item.key!,
+          label: `${item.val().date} ${item.val().title}: ${formatCurrency(
+            Math.abs(item.val().amount),
+          )}`,
+        })),
+    [uniqueData],
+  );
+
+  const servicingCharge = Number(
+    useObject(getDatabaseReference(`info/project/${id}/servicing`))[0]?.val() ??
+      0,
+  );
+
   return (
-    <SafeAreaView className="flex-1 bg-background">
-      <HeaderBar title={id} />
-      <ScrollView
-        className="bg-background p-2"
-        contentContainerStyle={{ flexGrow: 1 }}
-      >
-        {loading ? (
-          <View className="flex-1 items-center justify-center">
-            <Loading />
-          </View>
-        ) : error ? (
-          <View className="flex-1 items-center justify-center">
-            <ErrorUI error={error} />
-          </View>
-        ) : !data?.length ? (
-          <View className="flex-1 items-center justify-center">
-            <EmptyUI />
-          </View>
-        ) : (
-          <View className="gap-2 pb-4">
-            {uniqueData
-              .sort((a, b) => b.key!.localeCompare(a.key!))
-              .map((item) => (
-                <ProjectTransactionRow
-                  key={item.key}
-                  id={id}
-                  transactionData={item}
-                />
-              ))}
-          </View>
+    <>
+      <SafeAreaView className="flex-1 bg-background">
+        <HeaderBar
+          title={id}
+          right={
+            <TouchableOpacity
+              onPress={() => {
+                setOpenDialog(true);
+              }}
+            >
+              <ThemedIcon name="add-circle-outline" size={30} />
+            </TouchableOpacity>
+          }
+        />
+        <ScrollView
+          className="bg-background p-2"
+          contentContainerStyle={{ flexGrow: 1 }}
+        >
+          {loading ? (
+            <View className="flex-1 items-center justify-center">
+              <Loading />
+            </View>
+          ) : error ? (
+            <View className="flex-1 items-center justify-center">
+              <ErrorUI error={error} />
+            </View>
+          ) : !data?.length ? (
+            <View className="flex-1 items-center justify-center">
+              <EmptyUI />
+            </View>
+          ) : (
+            <View className="gap-2 pb-4">
+              {uniqueData
+                .sort((a, b) => b.key!.localeCompare(a.key!))
+                .map((item) => (
+                  <ProjectTransactionRow
+                    key={item.key}
+                    id={id}
+                    transactionData={item}
+                    servicingCharge={servicingCharge}
+                    paidArray={paidDataOptions}
+                    paidDataOptions={paidDataOptions}
+                    total={totalPayment}
+                  />
+                ))}
+            </View>
+          )}
+        </ScrollView>
+        {data && data.length > 0 && (
+          <TotalBalanceRow
+            value={totalValue}
+            date={balanceVal?.date}
+            error={balanceError?.message}
+          />
         )}
-      </ScrollView>
-      {data && data.length > 0 && (
-        <TotalBalanceRow
-          value={totalValue}
-          date={balanceVal?.date}
-          error={balanceError?.message}
+      </SafeAreaView>
+
+      {openDialog && (
+        <ProjectTransactionDialog
+          id={id}
+          paidDataOptions={paidDataOptions}
+          servicingCharge={servicingCharge}
+          open={openDialog}
+          onOpenChange={setOpenDialog}
         />
       )}
-    </SafeAreaView>
+    </>
   );
 }
