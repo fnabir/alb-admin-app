c93d688d1a75486a2ddf6e0d26e3b58b4d33dec9
Farhan Noor Abir <fnoor.abir@gmail.com>
2026-06-19 13:00:04 +0100

    Add the badge variants in web

--- packages/ui/src/badge/Badge.web.tsx
+++ packages/ui/src/badge/Badge.web.tsx
@@ -2,14 +2,32 @@ import { BadgeProps } from './types';
 
 export function Badge({
   label,
+  variant = 'default',
   className = '',
   textClassName = '',
 }: BadgeProps) {
+  const style = {
+    text: {
+      default: 'text-background',
+      success: 'text-green-50',
+      warning: 'text-yellow-50',
+      error: 'text-red-50',
+      light: 'text-black',
+    },
+    background: {
+      default: 'bg-primary',
+      success: 'bg-green-800',
+      warning: 'bg-yellow-800',
+      error: 'bg-red-800',
+      light: 'bg-white',
+    },
+  };
+
   return (
     <div
-      className={`w-fit bg-primary text-background text-sm lg:text-base px-2 lg:px-3 rounded-full font-semibold ${className}`}
+      className={`w-fit ${style.background[variant]} text-sm px-1.5 lg:px-2 rounded-full font-medium ${className}`}
     >
-      <span className={`${textClassName}`}>{label}</span>
+      <span className={`${style.text[variant]} ${textClassName}`}>{label}</span>
     </div>
   );
 }
